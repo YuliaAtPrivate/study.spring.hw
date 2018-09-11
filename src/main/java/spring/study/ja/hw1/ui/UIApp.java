f24f5820e56d352357848ad2add1f4edb3bf37ae
Yilia At <julia.atlasova@yandex.ru>
2018-09-11 05:11:04 +0300

    spring.xml file added but doesn't work.

--- src/main/java/spring/study/ja/hw1/ui/UIApp.java
+++ src/main/java/spring/study/ja/hw1/ui/UIApp.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.study.ja.hw1.service.AuditoriumManager;
 
 import javax.swing.*;
+import java.io.File;
 
 public class UIApp {
     public static AuditoriumManager auditoriumManager;
@@ -17,17 +18,28 @@ public class UIApp {
         //main frame view
         //--JFrame.setDefaultLookAndFeelDecorated(true);
 
-        JLabel label = new JLabel("Hello World");
+        JLabel label = new JLabel("try to get auditoriums");
+//        JLabel label1 = new JLabel(auditoriumManager.getAll().toString());
         frame.setSize(600, 400);
         frame.setLocationRelativeTo(null);
 
         frame.getContentPane().add(label);
+//        frame.getContentPane().add(label1);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("UIApp.xml");
-        auditoriumManager = (AuditoriumManager) ctx.getBean("auditoriumManager");
+        // spring/UIApp.xml
+        ///U./src/main/resources/spring/UIApp.xml
+        String fileName = "UIApp.xml";
+        File file2 = new File(fileName);
+        System.out.println(file2.exists());
+        System.out.println(file2.canRead());
+        System.out.println(file2.canWrite());
+
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(fileName);
+        //./src/main/resources/spring/UIApp.xml
+//        auditoriumManager = (AuditoriumManager) ctx.getBean("auditoriumManager");
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createGUI();
--- src/test/java/spring/study/ja/hw1/AuditoriumManagerTests.java
+++ src/test/java/spring/study/ja/hw1/AuditoriumManagerTests.java
@@ -1,7 +1,11 @@
 package spring.study.ja.hw1;
 
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.study.ja.hw1.service.AuditoriumManager;
+
+import java.io.File;
 
 public class AuditoriumManagerTests {
     @Test
@@ -10,5 +14,20 @@ public class AuditoriumManagerTests {
         AuditoriumManager manager = new AuditoriumManager(infoPath);
         manager.init();
         System.out.println(manager.auditoriums.size());
+    }
+
+    @Test
+    public void AuditoriumInfoForManagerCanBeReadFromSpringContext() {
+
+        File file1 = new File("./src/main/resources/data/auditoriums.info");
+        File file2 = new File("./src/main/resources/spring/UIApp.xml");
+        System.out.println(file1.exists());
+        System.out.println(file2.exists());
+
+
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("UIApp.xml");
+        //./src/main/resources/spring/UIApp.xml
+        AuditoriumManager auditoriumManager = (AuditoriumManager) ctx.getBean("auditoriumManager");
+        System.out.println(auditoriumManager.getAll().size());
     }
 }
